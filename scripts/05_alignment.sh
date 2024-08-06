96895c19855c1babce2076008189f27ca9eae6b5
Jonathan Kalami <116915872+itsjoeka@users.noreply.github.com>
2024-08-06 08:56:26 +0000

    Update 05_alignment.sh

--- scripts/05_alignment.sh
+++ scripts/05_alignment.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+
+TRIM_DIR="/home/jupyter/methylb/bisulfite-seq/trimbseq"
+GENOME_DIR="/home/jupyter/methylb/data/refDir"
+OUT_DIR="$/home/jupyter/methylb/bisulfite-seq/aligned"
+SAMPLE="SRR26949323"
+
+for sample in "${SAMPLES}"; do
+
+bismark \
+        --parallel 2 \
+        --genome "${GENOME_DIR}" \
+        -1 "${TRIM_DIR}"/"${sample}"_1_val_1.fq.gz \
+        -2 "${TRIM_DIR}"/"${sample}"_2_val_2.fq.gz
+        -o "${OUT_DIR}"
