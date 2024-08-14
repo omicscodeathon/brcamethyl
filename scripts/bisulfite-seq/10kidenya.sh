4be3990765270b14b68d70babe87787fb66f9837
itsjoeka <itsjoeka@icloud.com>
2024-08-14 22:20:04 +0000

    updated scripts

---
+++ scripts/bisulfite-seq/10kidenya.sh
@@ -0,0 +1,10 @@
+bismark_methylation_extractor -p --gzip --comprehensive --cytosine_report --merge_non_CpG --bedGraph --multicore 2 --counts --buffer_size 10G SRR26949325_1_val_1_bismark_bt2_pe.deduplicated.bam 
+
+
+
+
+
+
+
+
+bismark_methylation_extractor -p --gzip --comprehensive --cytosine_report --merge_non_CpG --bedGraph --multicore 2 --counts --buffer_size 10G SRR26949326_1_val_1_bismark_bt2_pe.deduplicated.bam 
